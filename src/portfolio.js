fe470659b560320040d69ecebf3d458cfecbb7ab
MuhammadAnwarBadat <anwarbadat1997.ab@gmail.com>
2024-11-24 23:03:11 +0500

    Updated portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -440,15 +440,6 @@ const blogSection = {
 //   display: true // Set false to hide this section, defaults to true
 // };
 
-// Resume Section
-const resumeSection = {
-  title: "Resume",
-  subtitle: "Feel free to download my resume",
-
-  // Please Provide with Your Podcast embeded Link
-  display: true // Set false to hide this section, defaults to true
-};
-
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
@@ -479,10 +470,10 @@ export {
   bigProjects,
   achievementSection,
   blogSection,
-  talkSection,
-  podcastSection,
+  // talkSection,
+  // podcastSection,
   contactInfo,
   twitterDetails,
-  isHireable,
-  resumeSection
+  isHireable
+  // resumeSection
 };
