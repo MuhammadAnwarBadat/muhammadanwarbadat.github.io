dcfe2ba566dd9ef4fdb5948e5e383997aa234a1a
MuhammadAnwarBadat <anwarbadat1997.ab@gmail.com>
2024-11-25 13:02:32 +0500

    Added gallery section

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -16,6 +16,7 @@ import ScrollToTopButton from "./topbutton/Top";
 import Twitter from "./twitter-embed/twitter";
 import Profile from "./profile/Profile";
 import TravellingGallery from "./TravellingGallery/TravellingGallery";  // Add this line
+import TravellingGallery from "./TravellingGallery/TravellingGallery";  // Add this line
 import SplashScreen from "./splashScreen/SplashScreen";
 import {splashScreen} from "../portfolio";
 import {StyleProvider} from "../contexts/StyleContext";
